5b9d97bedd739986c7ade3e28a3fea7550abba67
João Gabriel Passarello <velozopassarello@gmail.com>
2024-07-23 13:30:35 -0300

    rpg de texto

---
+++ Base.js
@@ -0,0 +1,2 @@
+let titulo = 'Bem-vindo ao novo mundo!';
+let intro = 'Você será transportado para um mundo de aventuras e desafios, onde voçê poderá trilhar o seu próprio caminho e torna-se o que quiser.';
